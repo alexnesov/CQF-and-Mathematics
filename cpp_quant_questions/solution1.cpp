805e836ad4844f35b49e79b91acda19b4131fc32
alexnesov <alexandrenesovic@gmail.com>
2022-01-30 10:14:12 +0100

    solution1 init

---
+++ cpp_quant_questions/solution1.cpp
@@ -0,0 +1,36 @@
+#include <iostream> 
+#include <vector> 
+#include <queue>
+
+
+class Solution {
+    public: 
+        int findKthLargest(std::vector<int>& nums, int k) {
+            std::priority_queue<int> max_heap;
+            for (int i = 0; i < nums.size(); ++i){
+                max_heap.push(nums[i]);
+                }
+                int j = 0;
+                while (j++ < k -1){
+                    max_heap.pop();
+                }
+                return max_heap.top();
+            }
+        };
+
+
+int main() {
+    // Creating first solution instance
+    Solution sol_one;
+
+
+    // Defining input vector
+    std::vector<int> vect_input {3 ,2 , 1 , 5 , 6 ,4};
+
+    // Testing the Solution
+    std::cout << "\n\n";
+    std::cout << sol_one.findKthLargest(vect_input, 2);
+    std::cout << "\n\n";
+    
+    return 0;
+}
